1404b3807d319ffd85a40497801570ea5153e335
Sergey Ivanov <Ivanov.sergey.v@russianpost.ru>
2023-01-25 12:23:02 +0300

    007 reverseInteger new algorithm

--- src/_007_reverseInteger/Solution.java
+++ src/_007_reverseInteger/Solution.java
@@ -1,25 +1,33 @@
 package _007_reverseInteger;
 
+
+//https://leetcode.com/problems/reverse-integer/submissions/884919422/
+// Solution from java.lang.Integer:648
+
 class Solution {
     public int reverse(int x) {
-        StringBuilder str = new StringBuilder();
-        boolean minus = x < 0;
+        final int radix = 10;
+        int result = 0;
+        boolean negative = x < 0;
+        int limit = negative ? -Integer.MAX_VALUE : Integer.MIN_VALUE;
+        int multmin = limit / radix;
+
         String s = String.valueOf(Math.abs(x));
-        if(minus)
-            str.append("-");
-        for (int i = s.length()-1; i >= 0; i--) {
-            str.append(s.charAt(i));
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int digit = Character.digit(s.charAt(i), radix);
+            if (digit < 0 || result < multmin || result < limit + digit) {
+                return 0;
+            }
+            result *= radix;
+            result -= digit;
         }
-        try{
-           return Integer.parseInt(str.toString()) ;
-        } catch (Exception e){
-           return 0;
-        }
+        return negative ? result : -result;
     }
 
     public static void main(String[] args) {
         System.out.println(
-                new Solution().reverse(1534236469)
+                new Solution().reverse(-123)
         );
     }
 }
