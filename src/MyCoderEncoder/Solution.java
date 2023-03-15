a75c79d1956409e8f1913635fac52e44dabbc0be
Sergey Ivanov <Ivanov.sergey.v@russianpost.ru>
2023-03-15 12:50:03 +0300

    update encode/decode func (provided uniform and more random selection of the cursor for the code)

--- src/MyCoderEncoder/Solution.java
+++ src/MyCoderEncoder/Solution.java
@@ -2,29 +2,20 @@ package MyCoderEncoder;
 
 import leetCode.common.Utils;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class Solution {
 
     public static void main(String[] args) {
-        //'\uFFFF' 65535
-        final String code = System.currentTimeMillis() +"";
-        final String INPUT_DATA = "Utils.countTime(() -> {\n" +
-                "            String data = INPUT_DATA;\n" +
-                "            data = encode(data, code);\n" +
-                "            System.out.println(data);\n" +
-                "\n" +
-                "            data = decode(data, code);\n" +
-                "            System.out.println(data);\n" +
-                "        });";
+        final String INPUT_DATA = "MY STRING";
+        final String CODE = "MY PASS IS VERY STRONG!";
 
         Utils.countTime(() -> {
             String data = INPUT_DATA;
-            data = encode(data, code);
+            data = encode(data, CODE);
             System.out.println(data);
 
-            data = decode(data, code);
+            data = decode(data, CODE);
             System.out.println(data);
         });
     }
@@ -46,34 +37,34 @@ public class Solution {
         char[] codes = codeString.toCharArray();
         char[] data = source.toCharArray();
 
-        final char hash1 = (char) data.length;
-        final char hash2 = getHashCode(codes);
-        char prevDec = (char) (hash1 + hash2);
+        final char hashData = (char) data.length;
+        final char hashCode = getHashCode(codes);
+        char prevDec = (char) (hashData + hashCode);
 
         for (int i = 0; i < data.length; i++) {
             char code = getCode(codes, prevDec, i); // get code from codes[]
-            char dec = getDecorator(hash1, hash2, i);
-//           System.out.println((int) dec);
+            char dec = getDecorator(hashData, hashCode, i);
             data[i] = func.countIt(data[i], code, dec);
-            prevDec = dec;
+            prevDec -= dec;
         }
         return String.valueOf(data);
     }
 
-    private static char getCode(char[] codes, char prevDec, int i) {
-        int cursor = getCursor(codes.length, prevDec);
-        return (char) (codes.length == 0 ? i : codes[cursor]);
+    private static char getCode(char[] codes, char prevDec, int iterator) {
+        int cursor = getCursor(codes.length, prevDec, iterator);
+        //System.out.println(cursor);
+        return (char) (codes.length == 0 ? iterator : codes[cursor]);
     }
 
-    private static int getCursor(int length, int prevDec) {
+    private static int getCursor(int length, int prevDec, int iterator) {
         if (length == 0) {
             return 0;
         }
-        return length - (prevDec % length) - 1;
+        return length - ((prevDec - iterator) % length) - 1;
     }
 
     private static char getDecorator(char hashData, char hashCode, int i) {
-        int val = hashCode + i * hashData ;
+        int val = hashCode + i * hashData;
         return (char) val;
     }
 
