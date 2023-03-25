347a77df189e638c9c787b2468e6fe5af4cb4001
Sergey Ivanov <Ivanov.sergey.v@russianpost.ru>
2023-03-25 12:57:34 +0300

    011 ContainerWithMostWater effective solution

--- src/leetCode/_011_ContainerWithMostWater/Solution.java
+++ src/leetCode/_011_ContainerWithMostWater/Solution.java
@@ -1,33 +1,28 @@
 package leetCode._011_ContainerWithMostWater;
 
+import static java.lang.Math.min;
+
 // https://leetcode.com/problems/container-with-most-water/
 //
 class Solution {
 
     public int maxArea(int[] height) {
-        int result = Integer.MIN_VALUE;
-
-        for (int i = 0; i < height.length; i++) {
-            for (int j = i + 1; j < height.length; j++) {
-                int limit = Math.min(height[i], height[j]);
-                int volume = (j - i) * limit;
-                result = Math.max(result, volume);
+        int i = 0, j = height.length - 1, area = 0;
+        while (i < j) {
+            int volume = Math.min(height[i], height[j]) * (j - i);
+            area = Math.max(area, volume);
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
             }
         }
-        return result;
-    }
-
-    private int countVolume(int[] height, int start, int end) {
-        int limit = Math.min(height[start], height[end]);
-        return (end - start) * limit;
+        return area;
     }
 
     public static void main(String[] args) {
         int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-        System.out.println(
-                new Solution().countVolume(height, 1, height.length - 1)
-        );
-        System.out.println("---------------");
+
         System.out.println(
                 new Solution().maxArea(height)
         );
